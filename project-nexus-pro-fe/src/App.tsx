c933270845627a1c02fc5df9be9651f10a5c1584
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-24 23:38:56 +0700

    feat: set up project structure with React and TypeScript

---
+++ project-nexus-pro-fe/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex gap-4 justify-center items-center">
+        <h1 className="text-4xl font-bold text-blue-500">Project Nexus Prodev FE</h1> 
+      </div>
+    </>
+  )
+}
+
+export default App
